7349557ffc3ef3f94ead4ad98b5638fdacada012
Hans Wannop <hanswannop@me.com>
2021-01-22 00:01:38 +0100

    Initial project setup for glfw and vulkan.

--- VulkanTutorial/main.cpp
+++ VulkanTutorial/main.cpp
@@ -5,10 +5,36 @@
 //  Created by null on 21/01/2021.
 //
 
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
+
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    glfwInit();
+    
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Coucou Vulkan", nullptr, nullptr);
+    uint32_t extensionCount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+    
+    std::cout << extensionCount << " extensions supported\n";
+    
+    glm::mat4 matrix;
+    glm::vec4 vec;
+    auto test = matrix * vec;
+    
+    while(!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+    
+    glfwDestroyWindow(window);
+    
+    glfwTerminate();
     return 0;
 }
