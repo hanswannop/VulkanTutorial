12fbcc40819105852155174a366d6b72cd9afbd9
Hans Wannop <hanswannop@me.com>
2021-01-23 03:12:35 +0100

    Creation of VKInstance

--- VulkanTutorial/main.cpp
+++ VulkanTutorial/main.cpp
@@ -5,9 +5,10 @@
 //  Created by null on 21/01/2021.
 //
 
-#define GLFW_INCLUDE_VULKAN
+
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wdocumentation"
+#define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 #pragma clang diagnostic pop
 
@@ -16,7 +17,6 @@
 #include <glm/vec4.hpp>
 #include <glm/mat4x4.hpp>
 
-#include <vulkan/vulkan.h>
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
@@ -28,6 +28,7 @@ class CoucouTriangleApplication {
 public:
     
     void run(){
+        initWindow();
         initVulkan();
         mainLoop();
         cleanup();
@@ -35,14 +36,19 @@ public:
     
 private:
     GLFWwindow* window;
+    VkInstance instance;
     
-    void initVulkan(){
+    void initWindow(){
         glfwInit();
         
-        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API); // Avoid GLFW creating OpenGL context
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE); // Deactivate window resising for now
         
         window = glfwCreateWindow(WIDTH, HEIGHT, "Coucou Vulkan", nullptr, nullptr);
+    }
+    
+    void initVulkan(){
+        createInstance();
         
         uint32_t extensionCount = 0;
         vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
@@ -60,8 +66,35 @@ private:
     }
     
     void cleanup(){
+        vkDestroyInstance(instance, nullptr);
         glfwDestroyWindow(window);
         glfwTerminate();
+    }
+    
+    void createInstance(){
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Coucou Vulkan";
+        appInfo.applicationVersion = VK_MAKE_VERSION(0, 1, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(0, 1, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_2;
+        
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+        
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions;
+        
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+        createInfo.enabledLayerCount = 0;
+        if(vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS){
+            throw std::runtime_error("Failed to create instance!");
+        }
+        
     }
 };
 
